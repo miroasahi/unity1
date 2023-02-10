0cd72530ce93d931df2f00e8279f8045925099d9
yusha-aaaa <ikomotmk5526@gmail.com>
2023-02-10 09:51:47 +0900

    エラーあり

--- Assets/animation/seigyo.cs
+++ Assets/animation/seigyo.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class seigyo : MonoBehaviour
 {
     // Start is called before the first frame update
+    Animator animator;
     void Start()
     {
+        anim = GetComponent<Animator>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("attack");
+        }
+        //else
+        {
+            //animator.SetTriggerl("attack");
+        }
+        
         
     }
 }
